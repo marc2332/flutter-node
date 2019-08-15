c8b30d639c7cc1a5bc52edadbb1e73b3c93319a0
Marc Espín <mespinsanz@gmail.com>
2019-08-15 10:52:03 +0200

    Init commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# Flutter-Node
+
+A NodeJS module to interact with the FLutter SDK
+
+Working now:
+- Get an array of the connected devices
+- Run an app from the source
+
+Made by Marc Espin
---
+++ test.js
@@ -0,0 +1,18 @@
+const Flutter = require("./index.js");
+
+Flutter.getDevices(function(array,err){
+    if(err){
+        console.log(err)
+        return err;
+    }
+    Flutter.run({
+        path:"C:\\Users\\mespi\\Desktop\\test_marc",
+        id:array[0][1]
+    },function(output,err){
+        if(err) {
+            console.log(err)
+            return err;
+        }
+        console.log(output)
+    })
+})
